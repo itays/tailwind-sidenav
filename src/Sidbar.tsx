01e22e2ababba02990707a78cbb1d99faa022001
Itay Sidis <isidis@chegg.com>
2022-02-18 16:44:00 +0200

    :construction: adding icons

--- src/Sidbar.tsx
+++ src/Sidbar.tsx
@@ -1,15 +1,19 @@
 import React from "react";
-
+import { FaFire, FaPoo } from "react-icons/fa";
+import { BsPlus, BsFillLightningFill, BsGearFill } from "react-icons/bs";
 function Sidebar() {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-white shadow-lg">
-      <i>A</i>
-      <i>B</i>
-      <i>C</i>
-      <i>D</i>
-      <i>E</i>
+      <SideBarIcon icon={<FaFire size={28} />} />
+      <SideBarIcon icon={<BsPlus size={32} />} />
+      <SideBarIcon icon={<BsFillLightningFill size={20} />} />
+      <SideBarIcon icon={<FaPoo size={20} />} />
     </div>
   );
 }
+
+const SideBarIcon = ({ icon }: any) => (
+  <div className="sidebar-icon">{icon}</div>
+);
 
 export default Sidebar;
